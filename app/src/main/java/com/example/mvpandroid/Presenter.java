9b4dc75e0c71355a61964c921de9921d64c605dc
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-17 23:33:49 +0300

    change Presenter

--- app/src/main/java/com/example/mvpandroid/Presenter.java
+++ app/src/main/java/com/example/mvpandroid/Presenter.java
@@ -7,4 +7,28 @@ public class Presenter {
         this.mModel = new Model();
         this.view = view;
     }
+    private int calcNewModelvalue(int modelElementIndex){
+        int currentvalue = mModel.getElementValueAtIndex(modelElementIndex);
+        return currentvalue+1;
+    }
+    public  void buttonClick(int btnIndex){
+        int newModelValue;
+        switch (btnIndex){
+            case R.id.btnCounter1:
+                newModelValue= calcNewModelvalue(0);
+                mModel.setElementValueAtIndex(0,newModelValue);
+                view.setButtonText(1,newModelValue);
+                break;
+            case R.id.btnCounter2:
+                newModelValue= calcNewModelvalue(1);
+                mModel.setElementValueAtIndex(1,newModelValue);
+                view.setButtonText(2,newModelValue);
+                break;
+            case R.id.btnCounter3:
+                newModelValue= calcNewModelvalue(2);
+                mModel.setElementValueAtIndex(2,newModelValue);
+                view.setButtonText(3,newModelValue);
+                break;
+        }
+    }
 }
