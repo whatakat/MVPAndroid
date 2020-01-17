4a711b0ca64851dc6fa0fec9aba276e293facf69
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-17 23:13:14 +0300

    change MainActivity

--- app/src/main/java/com/example/mvpandroid/MainActivity.java
+++ app/src/main/java/com/example/mvpandroid/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.mvpandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainView, View.OnClickListener {
+    private Button btnCounter1;
+    private Button btnCounter2;
+    private Button btnCounter3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
