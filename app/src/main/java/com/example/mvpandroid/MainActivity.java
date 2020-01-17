8deda3be609fe79d975a70646bd25212102753b4
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-17 23:25:15 +0300

    add setButtonText

--- app/src/main/java/com/example/mvpandroid/MainActivity.java
+++ app/src/main/java/com/example/mvpandroid/MainActivity.java
@@ -10,11 +10,39 @@ public class MainActivity extends AppCompatActivity implements MainView, View.On
     private Button btnCounter1;
     private Button btnCounter2;
     private Button btnCounter3;
+    private Presenter mPresenter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCounter1 = (Button)findViewById(R.id.btnCounter1);
+        btnCounter2 = (Button)findViewById(R.id.btnCounter2);
+        btnCounter3 = (Button)findViewById(R.id.btnCounter3);
+        btnCounter1.setOnClickListener(this);
+        btnCounter2.setOnClickListener(this);
+        btnCounter3.setOnClickListener(this);
+        mPresenter = new Presenter(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void setButtonText(int btnIndex, int value) {
+        switch (btnIndex){
+            case 1:
+                btnCounter1.setText("Amount = "+ value);
+                break;
+            case 2:
+                btnCounter2.setText("Amount = "+value);
+                break;
+            case 3:
+                btnCounter3.setText("Amount = "+value);
+                break;
+        }
     }
 }
